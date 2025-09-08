667f9720b6cc0487b78539ebf7c79753c918c189
Loc <loc.tntran@gmail.com>
2025-09-08 15:43:08 -0500

    P03 full_credit HW

---
+++ P03/full_credit/Card.java
@@ -0,0 +1,27 @@
+package P03.full_credit;
+
+public class Card {
+    private String term;
+    private String definition;
+
+    public Card(String term, String definition) {
+        if ( term == null || term.isEmpty() || definition == null || definition.isEmpty()) {
+            throw new IllegalArgumentException("Term and definition cant be null or empty");
+        }
+        this.term = term;
+        this.definition = definition;
+    }
+
+    public boolean attempt(String response){
+        return term.toUpperCase().equals(response.toUpperCase());
+    }
+
+    public String getTerm(){
+        return this.term;
+    }
+
+    @Override
+    public String toString() {
+        return this.definition;
+    }
+}
