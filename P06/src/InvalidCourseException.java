fe53cef25dae2d7692e3a5d913afe545c2836177
Loc <loc.tntran@gmail.com>
2025-10-06 21:30:35 -0500

    Add space for nothing

--- P06/src/InvalidCourseException.java
+++ P06/src/InvalidCourseException.java
@@ -2,10 +2,10 @@ package P06.src;
 
 public class InvalidCourseException extends IllegalArgumentException{
     public InvalidCourseException(String dept) {
-        super("Invalid dept in new Course:" + dept);
+        super("Invalid dept in new Course: " + dept);
     }
 
     public InvalidCourseException(String dept, int num) {
-        super("Invalid course number " + num + " in new Course:" + dept);
+        super("Invalid course number " + num + " in new Course: " + dept);
     }
 }
