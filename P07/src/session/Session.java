1476e1a9e029b082e90a726d074db679825ea53e
Loc <loc.tntran@gmail.com>
2025-10-13 20:49:02 -0500

    Java Doc for Session

--- P07/src/session/Session.java
+++ P07/src/session/Session.java
@@ -6,26 +6,61 @@ import P07.src.people.Tutor;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Tutoring Session
+ * @author Loc Tran
+ * @version 1.0
+ * @since 1.0
+ */
 public class Session {
     private Course course;
     private DateRange dates;
     private Tutor tutor;
     private List<Student> students;
 
+    /**
+     * Constructor
+     * @param course the course
+     * @param tutor the tutor
+     */
     public Session(Course course, Tutor tutor) {
         this.course = course;
         this.tutor = tutor;
         this.students = new ArrayList<>();
     }
 
-    public void setSchedule(String date, String startTime, String duration){
+    /**
+     * Set schedule for the session by time
+     * @param date YYYY:MM:DD
+     * @param startTime HH:MM
+     * @param endTime HH:MM
+     */
+    public void setSchedule(String date, String startTime, String endTime){
+        this.dates = new DateRange(date, startTime, endTime);
+    }
+
+    /**
+     * Set schedule for the session by duration
+     * @param date YYYY:MM:DD
+     * @param startTime HH:MM
+     * @param duration MM
+     */
+    public void setSchedule(String date, String startTime, long duration){
         this.dates = new DateRange(date, startTime, duration);
     }
 
+    /**
+     * add students to the session
+     * @param student the student
+     */
     public void addStudent(Student student){
         students.add(student);
     }
 
+    /**
+     * Return String representation of Session
+     * @return return the string formatted of the Session
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
